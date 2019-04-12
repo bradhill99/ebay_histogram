3146e9b8d7c429824b391a0993c329435896dbe7
Brad Liu <brad_liu@trend.com.tw>
2019-04-12 19:47:27 +0800

    use aspet filter search instead of keyword

--- lib.py
+++ lib.py
@@ -27,13 +27,12 @@ def get_token():
 
     return json.loads(r.text)['access_token']
 
-def query_completed_items(access_token, start_date, end_date, pagination = 1):
+def do_query(access_token, start_date, end_date, pagination):
     params = {
         'OPERATION-NAME':'findCompletedItems',
         'SERVICE-VERSION':'1.7.0',
         'SECURITY-APPNAME':os.environ['CLIENT_ID'],
         'RESPONSE-DATA-FORMAT':'JSON',
-        'keywords':'hobby box -break',
         'categoryId':'212',
         'itemFilter(0).name':'SoldItemsOnly',
         'itemFilter(0).value':'true',
@@ -42,7 +41,9 @@ def query_completed_items(access_token, start_date, end_date, pagination = 1):
         'itemFilter(2).name':'EndTimeTo',
         'itemFilter(2).value':end_date,
         'sortOrder':'EndTimeSoonest',
-        'paginationInput.pageNumber':pagination,   
+        'paginationInput.pageNumber':pagination,
+        'aspectFilter(0).aspectName':'Product',
+        'aspectFilter(0).aspectValueName':'Box'
     }
     r = requests.get(
         os.environ['FINDING_API'],
@@ -53,23 +54,51 @@ def query_completed_items(access_token, start_date, end_date, pagination = 1):
     )
 
     if r.status_code != 200:
-        return ""
+        raise ValueError('http error, status code={}'.format(r.status_code))
 
     r_dict = json.loads(r.text)
-    # should improve error handling for this part, findCompletedItemsResponse and searchResult may be empty
-    if 'item' in r_dict['findCompletedItemsResponse'][0]['searchResult'][0]:
-        item = r_dict['findCompletedItemsResponse'][0]['searchResult'][0]['item']
-    else:
-        return ""
+    try:
+        r_dict['findCompletedItemsResponse'][0]['searchResult'][0]['item'] = post_process(r_dict['findCompletedItemsResponse'][0]['searchResult'][0]['item'])
+    except:
+        print(r.text)
+        raise ValueError('key exception')
 
-    total_page = int(r_dict['findCompletedItemsResponse'][0]['paginationOutput'][0]['totalPages'][0])
-    if pagination < total_page:
-        pagination += 1
-        result = query_completed_items(access_token, start_date, end_date, pagination)
-        item.append(result)
-        return item
-    else:
-        return item
+    print("item size={}".format(len(r_dict['findCompletedItemsResponse'][0]['searchResult'][0]['item'])))
+    return r_dict
+
+def post_process(items):
+    # remove item with title has "break"
+    # add @timestamp key
+    filter_items = [ item for item in items if "break" not in item['title'][0].lower() ]
+    add_timestamp_field(filter_items)
+    return filter_items
+
+def add_timestamp_field(items):
+    for item in items:
+        item['@timestamp'] = item['listingInfo'][0]['endTime'][0]
+
+def query_completed_items(access_token, start_date, end_date):
+    pagination = 1
+    # total_items = list()
+    try:
+        # get first query and number of pages
+        result = do_query(access_token, start_date, end_date, pagination)
+        total_page = int(result['findCompletedItemsResponse'][0]['paginationOutput'][0]['totalPages'][0])
+        # total_items.extend(result['findCompletedItemsResponse'][0]['searchResult'][0]['item'])
+        save_result(result['findCompletedItemsResponse'][0]['searchResult'][0]['item'])
+        print("currnt page={}, total pages={}".format(pagination, total_page))
+
+        while pagination < total_page:
+            pagination += 1
+            result = do_query(access_token, start_date, end_date, pagination)
+            save_result(result['findCompletedItemsResponse'][0]['searchResult'][0]['item'])
+            print("currnt page={}, total pages={}".format(pagination, total_page))
+            # total_items.extend(result['findCompletedItemsResponse'][0]['searchResult'][0]['item'])
+    except Exception as e:
+        print('got exception, break the loop:{}'.format(e))
+
+    # print("total_items size={}".format(len(total_items)))
+    # return total_items
 
 def get_query_time_range(start_date):
     from datetime import datetime
@@ -80,8 +109,9 @@ def get_query_time_range(start_date):
     end_date = (date_time_obj+timedelta(days=1)).strftime("%Y-%m-%dT00:00:00.000Z")
     return (start_date, end_date)
 
-def save_result(result, start_date):
-    result_file = open('{}.txt'.format(start_date), 'w')
+def save_result(result):
+    start_date = sys.argv[1]
+    result_file = open('{}.txt'.format(start_date), 'a')
     for item in result:
         result_file.write('{}\n'.format(json.dumps(item)))
 
@@ -93,5 +123,5 @@ Usage FINDING_API=<> CLIENT_ID=<> CLIENT_SECRET=<> TOKEN_URL=<> lib.py <YYYY-MM-
 if __name__ == "__main__":
     token = get_token()
     (start_date, end_date) = get_query_time_range(sys.argv[1])
-    result = query_completed_items(token, start_date, end_date)
-    save_result(result, sys.argv[1])
+    query_completed_items(token, start_date, end_date)
+    # save_result(result, sys.argv[1])
